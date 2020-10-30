f5b89867626c8b53d7107706ec686f58ad9f00bf
Alekhin Sergey <alekhin1991@gmail.com>
2020-10-30 13:23:43 +0300

    update pagination on question page, add footer, update question template

--- app/views.py
+++ app/views.py
@@ -26,55 +26,74 @@ def pagination(object_list, request, per_page=10):
 questions = []
 for i in range(1, 20):
     questions.append({
-        'title': 'title ' + str(i),
+        'title': 'Title ' + str(i),
+        'author': 'author ' + str(i),
+        'date': 'date ' + str(i),
         'id': i,
         'answer': str(i),
         'tags': ['tag' + str(i), 'tag' + str(i + 1)],
+        'answers': [i + 1, i + 2],
         'rating': str(i),
         'text': 'text ' + str(i)
     })
 
 questions.append(({
-    'title': 'title ' + str(21),
+    'title': 'Title ' + str(21),
+    'author': 'author ' + str(21),
+    'date': 'date ' + str(21),
     'id': 21,
     'answer': str(21),
-    'tags': ['tag1'],
+    'tags': ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8', 'tag9', 'tag10', 'tag11', 'tag12', 'tag13',
+             'tag14', 'tag15', 'tag16', 'tag17', 'tag18'],
+    'answers': [18, 19, 20, 22, 23, 24, 25],
     'rating': str(21),
     'text': 'text ' + str(21)
 }))
 
 questions.append(({
-    'title': 'title ' + str(22),
+    'title': 'Title ' + str(22),
+    'author': 'author ' + str(22),
+    'date': 'date ' + str(22),
     'id': 22,
     'answer': str(22),
     'tags': ['tag1'],
+    'answers': [23],
     'rating': str(22),
     'text': 'text ' + str(22)
 }))
 
 questions.append(({
-    'title': 'title ' + str(23),
+    'title': 'Title ' + str(23),
+    'author': 'author ' + str(23),
+    'date': 'date ' + str(23),
     'id': 23,
     'answer': str(23),
     'tags': ['tag1'],
+    'answers': [24],
     'rating': str(23),
     'text': 'text ' + str(23)
 }))
 
 questions.append(({
-    'title': 'title ' + str(24),
+    'title': 'Title ' + str(24),
+    'author': 'author ' + str(24),
+    'date': 'date ' + str(24),
     'id': 24,
     'answer': str(24),
     'tags': ['tag2'],
+    'answers': [25],
     'rating': str(24),
     'text': 'text ' + str(24)
 }))
 
 questions.append(({
-    'title': 'title ' + str(25),
+    'title': 'Title ' + str(25),
+    'author': 'author ' + str(25),
+    'date': 'date ' + str(25),
     'id': 25,
     'answer': str(25),
     'tags': ['tag3'],
+    'answers': [26],
     'rating': str(25),
     'text': 'text ' + str(25)
 }))
@@ -99,20 +118,34 @@ def findId(val, id):
     return content
 
 
+def findAnswers(val, id):
+    content = []
+    for question in val:
+        if question['id'] == id:
+            for answer_id in question['answers']:
+                for answer in val:
+                    if answer['id'] == answer_id:
+                        content.append(answer)
+
+    return content
+
+
 def index(request):
+    content = pagination(questions, request)
     return render(request, 'index.html', {
         'title': 'New questions',
         'user_login': user,
-        'questions': pagination(questions, request),
+        'questions': content,
         **tags_users
     })
 
 
 def hot_questions(request):
+    content = pagination(questions, request)
     return render(request, 'index.html', {
         'title': 'Hot questions',
         'user_login': user,
-        'questions': pagination(questions, request),
+        'questions': content,
         **tags_users
     })
 
@@ -126,19 +159,23 @@ def new_question(request):
 
 
 def question(request, id):
+    content = findId(questions, id)
+    answer = pagination(findAnswers(questions, id), request, per_page=3)
     return render(request, 'question_page.html', {
-        'title': f'question {id}',
+        'title': f'Question {id}',
         'user_login': user,
-        'questions': findId(questions, id),
+        'questions': content,
+        'answers': answer,
         **tags_users
     })
 
 
 def tag(request, tag):
+    content = pagination(findTag(questions, tag), request)
     return render(request, 'index.html', {
         'title': f'Tag: {tag}',
         'user_login': user,
-        'questions': pagination(findTag(questions, tag), request),
+        'questions': content,
         **tags_users
     })
 
