590a46064eb8da94107df3840797bb64c981c61a
Alekhin Sergey <alekhin1991@gmail.com>
2021-01-11 22:08:31 +0300

    release config

--- askme/settings.py
+++ askme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i%1m*&683e$f@_eks%z5tyzik55e=buibon!b)@1mni-4jgvn&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'askme.wsgi.application'
 
 
 # SQLite3 settings
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # PostgreSQL settings
 # DATABASES = {
@@ -97,7 +97,7 @@ DATABASES = {
 # }
 
 # Heroku PostgreSQL settings
-# DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
